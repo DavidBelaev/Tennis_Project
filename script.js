bc132d56e044a5143daceede16d1df43c69493da
DavidBelaev <sialwtealcl@gmail.com>
2017-06-28 23:27:10 +0300

    Fixed mistakes

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
+/*
+	You had 2 mistakes.
+	First: your wait() function is not working.
+	Second: You set var time EQUAL to the string, instead of adding it.
+*/
+
 var a = prompt("how many beers did u buy?")
 displayTime();
 function displayTime() {
 	var time = ""
-	var b = a;
-	for(var i = a; a>-1; a--){
-		if( a != 0){
-		time = a + "bottles of beer on the wall, "+ a +" bottles of beer. "+
-		"Take one down and pass it around, " + (a-1) +" bottles of beer on the wall."
-		}else{
-		time = "No more bottles of beer on the wall, no more bottles of beer. " +
-		"Go to the store and buy some more, "+b+ " bottles of beer on the wall.";
+	for(var i = a; i >- 1; i--){
+		if(i != 0){
+			time += i + " bottles of beer on the wall, " + i + " bottles of beer. " +
+			"Take one down and pass it around, " + (i-1) + " bottles of beer on the wall."
 		}
+		else {
+			time += "No more bottles of beer on the wall, no more bottles of beer. " +
+			"Go to the store and buy some more, " + a + " bottles of beer on the wall.";
+		}
+		
+		time += "\n";
+		
 		document.getElementById("time").innerHTML = time;
 		wait(1000);
 	}
 }
 function wait(ms){
-   var start = new Date().getTime();
-   var end = start;
-   while(end < start + ms) {
-     end = new Date().getTime();
-  }
+	var start = new Date().getTime();
+	var end = start;
+
+	while(end < start + ms) {
+		end = new Date().getTime();
+	}
 }
