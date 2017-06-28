1ec53746240bb7cb081fdbcbd89cb0b1eb8d9a5e
DavidBelaev <sialwtealcl@gmail.com>
2017-06-28 22:10:24 +0300

    Created current time displayer

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-
 setInterval(displayTime, 1000)
-var a = 0;
+
 function displayTime() {
-	a++;
-	document.getElementById("time").innerHTML = a;
+	var date = new Date();
+	
+	var hour = date.getHours();
+	var minute = date.getMinutes();
+	var second = date.getSeconds();
+	
+	var time = "";
+	var times = [hour, minute, second];
+	for(var i = 0; i < 3; i++) {
+		time += String( times[i] );
+		
+		if(i !== 2) {
+			time += " : ";
+		}
+	}
+	
+	document.getElementById("time").innerHTML = time;
 }
